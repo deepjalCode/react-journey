84c9d24decafc491ada981e83dbd1e923c6d935f
deepjalCode <deepjalofficial@gmail.com>
2026-01-09 10:12:38 +0530

    child to parent data transfer

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Footer from './components/Footer'
-import Services from './pages/Services'
-import Product from './pages/Product'
-import Contact from './pages/Contact'
-import NotFound from './pages/NotFound'
-import New from './pages/New'
-import Oldest from './pages/Oldest'
-import Trend from './pages/Trend'
-import Details from './pages/Details'
 
 const App = () => {
 
+  const [theme, setTheme] = useState('dark')
   return (
-    <div className='h-screen w-full bg-emerald-700'>
-      <Navbar />
-      <Footer />
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/product' element={<Product />}>
-          <Route path='/product/new' element={<New />} />
-          <Route path='/product/old' element={<Oldest />} />
-          <Route path='/product/trend' element={<Trend />} />
-        </Route>
-        <Route path='/product/:id' element={<Details />} />
-        <Route path='/services' element={<Services />}/>
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/*' element={<NotFound />} />
-      </Routes>
-    
-      
+    <div>
+      <Navbar theme={theme} setTheme={setTheme} />
+      <h1>Theme is {theme}</h1>
     </div>
-    
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = (props) => {
+ 
+  
+  const changeTheme = ()=>{
+    props.setTheme(props.theme=='light'?'dark':'light')
+  }
+
   return (
-    <div className='flex bg-emerald-200 items-center justify-between px-8 py-4'>
-      <h2 className='font-bold text-2xl underline'>DCode</h2>
-      <div className='flex items-center gap-8'>
-        <Link className='textr-xl font-medium ' to='/'>Home</Link>
-        <Link className='textr-xl font-medium ' to='/product'>Product</Link>
-        <Link className='textr-xl font-medium ' to='/services'>Services</Link>
-        <Link className='textr-xl font-medium ' to='/contact'>Contact</Link>
-      </div>
+    <div>
+      <p>{props.theme}</p>
+      <button 
+        onClick={changeTheme}
+        className='bg-emerald-500 rounded px-5 py-2'>Change Theme</button>
     </div>
   )
 }
